29f7ca6c5dd51f717a29742bf0ec0cc250c755c0
s18b711 <s18b711@shohoku.ac.jp>
2019-10-10 15:10:22 +0900

    カスタムview

---
+++ app/src/main/java/jp/ac/shohoku/s18b706/ueno/screentransition/MainView.java
@@ -0,0 +1,25 @@
+package jp.ac.shohoku.s18b706.ueno.screentransition;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MainView extends View {
+    public MainView(Context context) {
+        super(context);
+    }
+
+    public MainView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MainView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MainView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
